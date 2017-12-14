5b8746ac0a2642cb194aa0c6a9a65de606ec87c4
swanand keskar <swanand.keskar@gmail.com>
2017-12-14 12:50:59 +0530

    make it better

--- index.js
+++ index.js
@@ -6,24 +6,32 @@ var pg = require('pg');
 
 var conString="postgres://eocivqdgabjhlc:223a89d1fc3932751d27389032fc8c7055d23879ef40302e23fffd5e44fdcfce@ec2-54-225-88-191.compute-1.amazonaws.com:5432/d66k8msq48v2b5";
 
+const { Pool, Client } = require('pg')
+const connectionString = 'postgres://eocivqdgabjhlc:223a89d1fc3932751d27389032fc8c7055d23879ef40302e23fffd5e44fdcfce@ec2-54-225-88-191.compute-1.amazonaws.com:5432/d66k8msq48v2b5'
 
+const pool = new Pool({
+  connectionString: connectionString,
+})
 
+pool.query('SELECT NOW()', (err, res) => {
+  console.log(err, res)
+  pool.end()
+})
 
+const client = new Client({
+  connectionString: connectionString,
+})
 
 app.get("/",function(req,res){
 
   //  res.send("welcome to node on heroku");
-  pg.connect(conString, function(err, client, done) {
-    console.log(err+"!!!!!!!!!!!!!!!");
-   client.query('SELECT NOW()', function(err, result) {
-     done();
-     if(err) return console.error(err);
-     console.log(result.rows);
-     res.send(result.rows);
-   });
- });
+ 
+  client.connect()
   
-   
+  client.query('SELECT NOW()', (err, res) => {
+    console.log(err, res)
+    client.end()
+  })
 });
 
 
