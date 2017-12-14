63c407fe1c354127c4bbd1be7061c13e324f3574
swanand keskar <swanand.keskar@gmail.com>
2017-12-14 18:48:45 +0530

    making it better

--- controller/taskController.js
+++ controller/taskController.js
@@ -20,7 +20,26 @@ exports.list_all_tasks =function(request,response){
 }
 
 exports.create_task =function(request,response){
-  const data = {uid: request.body.uid, name : request.body.name};
+  const data = {uid: request.body.uid, name : request.body.name
+  ,description : request.body.description
+  ,amount : request.body.amount};
+
+  pool.query('INSERT INTO TASKS(uid, name,description,amount) values($1, $2, $3, $4)',
+  [data.uid, data.name,data.description,data.amount],(err,res)=>{
+    if (err) {
+      console.log(err.stack)
+
+      response.json({"error":err});
+    } else {
+      console.log(res.rows[0])
+
+      response.status(200).json({"result":res.rows[0]});
+      // { name: 'brianc', email: 'brian.m.carlson@gmail.com' }
+    
+    
+    }
+  });
+
   response.status(200).json({"test":data});
         
 }
