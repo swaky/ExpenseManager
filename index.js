1083a41ddd639a23d3bd78753d78130cc4077b16
swanand keskar <swanand.keskar@gmail.com>
2017-12-14 11:23:48 +0530

    connection to db

--- index.js
+++ index.js
@@ -3,9 +3,32 @@ var express = require("express");
 var app=express();
 var port=process.env.PORT || 3000;
 
+
+const { Client } = require('pg');
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
+});
+
+
+
 app.get("/",function(req,res){
 
-    res.send("welcome to node on heroku");
+  //  res.send("welcome to node on heroku");
+
+    client.connect();
+    
+    client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+      if (err) throw err;
+      for (let row of res.rows) {
+        console.log(JSON.stringify(row));
+    res.send(JSON.stringify(row))  
+    }
+      client.end();
+    });
 });
+
+
 
 app.listen(port);
