5bc57093f36cf6f8f4bb8cdcd57c4903bb7b1d01
swanand keskar <swanand.keskar@gmail.com>
2017-12-14 12:11:21 +0530

    connection to db

--- index.js
+++ index.js
@@ -2,31 +2,28 @@ var express = require("express");
 
 var app=express();
 var port=process.env.PORT || 3000;
+var pg = require('pg');
+
+var conString="postgres://eocivqdgabjhlc:223a89d1fc3932751d27389032fc8c7055d23879ef40302e23fffd5e44fdcfce@ec2-54-225-88-191.compute-1.amazonaws.com:5432/d66k8msq48v2b5";
 
 
-const { Client } = require('pg');
-
-const client = new Client({
-  connectionString: process.env.DATABASE_URL,
-  ssl: true,
-});
 
 
 
 app.get("/",function(req,res){
 
   //  res.send("welcome to node on heroku");
-
-    client.connect();
-    
-    client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
-      if (err) throw err;
-      for (let row of res.rows) {
-        console.log(JSON.stringify(row));
-    res.send(JSON.stringify(row))  
-    }
-      client.end();
-    });
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    console.log(err+"!!!!!!!!!!!!!!!");
+   client.query('SELECT * FROM your_table', function(err, result) {
+     done();
+     if(err) return console.error(err);
+     console.log(result.rows);
+     res.send(result.rows);
+   });
+ });
+  
+   
 });
 
 
