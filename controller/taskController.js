f7fd459608183a4af3a7dcfa28ef7df9a001e82f
swanand keskar <swanand.keskar@gmail.com>
2017-12-14 19:26:04 +0530

    making it better

--- controller/taskController.js
+++ controller/taskController.js
@@ -13,7 +13,7 @@ exports.list_all_tasks =function(request,response){
 
     pool.query('SELECT * FROM TASKS', (err, res) => {
         console.log(err, res)
-        response.status(200).json({"expenses":res.rows});
+        response.status(200).json({"expenses":res.rows[0]});
        // client.end()
       });
     
