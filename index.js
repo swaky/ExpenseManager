dad3982d1268892a668b410fd98f970b5a2791e4
swanand keskar <swanand.keskar@gmail.com>
2017-12-14 16:43:59 +0530

    make it better

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ var app=express();
 var port=process.env.PORT || 3000;
 var pg = require('pg');
 
-var conString="postgres://eocivqdgabjhlc:223a89d1fc3932751d27389032fc8c7055d23879ef40302e23fffd5e44fdcfce@ec2-54-225-88-191.compute-1.amazonaws.com:5432/d66k8msq48v2b5";
 
 const { Pool, Client } = require('pg')
-const connectionString = 'postgres://eocivqdgabjhlc:223a89d1fc3932751d27389032fc8c7055d23879ef40302e23fffd5e44fdcfce@ec2-54-225-88-191.compute-1.amazonaws.com:5432/d66k8msq48v2b5'
+//const connectionString = 'postgres://eocivqdgabjhlc:223a89d1fc3932751d27389032fc8c7055d23879ef40302e23fffd5e44fdcfce@ec2-54-225-88-191.compute-1.amazonaws.com:5432/d66k8msq48v2b5'
 
+const connectionString =process.env.DATABASE_URL;
 const pool = new Pool({
   connectionString: connectionString,
 })
@@ -30,7 +30,7 @@ app.get("/",function(request,response){
   
   pool.query('SELECT * FROM TASKS', (err, res) => {
     console.log(err, res)
-    response.status(200).json({"expenses":res.rows,"connection":process.env.DATABASE_URL});
+    response.status(200).json({"expenses":res.rows});
    // client.end()
   })
 });
